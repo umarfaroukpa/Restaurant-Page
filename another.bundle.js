97948c7f4d3ece89ac6606e7a6bd57875e88e0c8
Umar Farouk <yasmarfaq@yahoo.com>
2025-04-23 16:25:54 +0100

    Updates

--- another.bundle.js
+++ another.bundle.js
@@ -1,2 +1,2 @@
-"use strict";(self.webpackChunkrestaurant_page=self.webpackChunkrestaurant_page||[]).push([[332],{496:(e,a,s)=>{var n=s(543),o=s.n(n);console.log(o().join(["Another","module","loaded!"]," "))}},e=>{e.O(0,[804],(()=>(496,e(e.s=496)))),e.O()}]);
+"use strict";(self.webpackChunkrestaurant_page=self.webpackChunkrestaurant_page||[]).push([[332],{496:(e,a,s)=>{var n=s(543),o=s.n(n);console.log(o().join(["Another","module","loaded!"]," "))}},e=>{e.O(0,[96],(()=>e(e.s=496))),e.O()}]);
 //# sourceMappingURL=another.bundle.js.map
